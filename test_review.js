90d51c8f8e41328a535744c31d5a6f6203a2acc3
Talon Navarez <navareztalon@gmail.com>
2016-11-23 09:10:12 -1000

    first commit

---
+++ test_review.js
@@ -0,0 +1,42 @@
+//JAVASCRIPT TEST REVIEW//
+//Talon Boyd Kauhanemakamaeonalahuihenui Navarez//
+//11232016//
+
+//Variables//
+var knight = "Arthur"
+var maiden = "Morgana"
+var monster = "Dragon"
+//Array//
+Array weaponchest = "Mace","Sorcerer's Stone","Excalibur"
+//Object//
+Object hero
+//Hail the King//
+function SwordInTheStone(w){
+	if worthy = ("Arthur"){
+		console.log("We have found our King" + worthy)
+	}else{
+		console.log("We must find keep searching, for" + worthy + "is unworthy")
+	}
+}function SwordInTheStone
+
+//Hail the King and Queen//
+function TrueLove(k,q){
+	if king + queen = "guinevere","Arthur"
+		console.log("On this day, our king and queen shall rule happily ever after!"){
+	}else{
+		console.log("We must keep searching, for true love must reign over Camelot")
+	}
+}
+
+//Merlin and Monster//
+function EpicBattle(c,e,p){
+	if power <= 15{
+		console.log("OUR WIZARD HAS FALLEN!!!")
+	}
+	
+	else if power >= 16 <= 40{
+		console.log("WIZARD HAS ALMOST SLAIN ENEMY!!!")
+	}else{
+		console.log("OUR WIZARD HAS SLAIN THE ENEMY!!!")
+	}
+}
